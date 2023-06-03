8ccfb2518895f03fdb36d3d68f44f544f26a7e3c
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-06-03 19:29:56 +0300

    hw3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import React, { useState } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { ContactFilter } from './ContactFilter/ContactFilter';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  
+  const deleteContact = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm contacts={contacts} setContacts={setContacts} />
+      <h2>Contacts</h2>
+      <ContactFilter filter={filter} setFilter={setFilter} />
+      <ContactList contacts={contacts} filter={filter} deleteContact={deleteContact} />
     </div>
   );
 };
